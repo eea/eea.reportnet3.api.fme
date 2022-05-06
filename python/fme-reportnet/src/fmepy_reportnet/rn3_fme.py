2c56bdd3323b77e6bc2b9d23b01e6192d2394f73
Peter Segerstedt <peter.segerstedt@sweco.se>
2022-05-06 09:22:05 +0200

    added resolving of v2 params

--- python/fme-reportnet/src/fmepy_reportnet/rn3_fme.py
+++ python/fme-reportnet/src/fmepy_reportnet/rn3_fme.py
@@ -13,14 +13,22 @@ class _RN3CredV0Mixin:
 class _RN3CredV1Mixin:
     VERSION: str = '1'
 @dataclass
+class _RN3CredV2Mixin:
+    VERSION: str = '2'
+    DATAFLOW_ID: str = None
+@dataclass
 class Reportnet3CredentialsV0(Reportnet3Credentials,_RN3CredV0Mixin):
     pass
 @dataclass
 class Reportnet3CredentialsV1(Reportnet3Credentials,_RN3CredV1Mixin,_RN3CredV0Mixin):
     pass
+@dataclass
+class Reportnet3CredentialsV2(Reportnet3Credentials,_RN3CredV2Mixin,_RN3CredV0Mixin):
+    pass
 _RN3CRED_VERSIONS = {
       '0': Reportnet3CredentialsV0
     , '1': Reportnet3CredentialsV1
+    , '2': Reportnet3CredentialsV2
 }
         
 
@@ -29,10 +37,15 @@ def resolve_named_connection(name: str) -> Reportnet3Credentials:
 
     Arguments:
     name -- Either the name of an FME Named Connection *or* a url
-            in the form <api_url>?API_KEY=<token>[&PROVIDER_ID=<provider_id>][&VERSION=<version>].
+            in the form 
+            
+            * version 0-1: <api_url>?API_KEY=<token>[&PROVIDER_ID=<provider_id>][&VERSION=<version>]
+            * version 2: <api_url>?VERSION=2&API_KEY=<token>&DATAFLOW_ID=<dataflow_id>[&PROVIDER_ID=<provider_id>]
 
             Example:
-                https://rn3api.eionet.europa.eu?VERSION=1&API_KEY=502982a2-95a5-43ae-bf3b-d16356042c86&PROVIDER_ID=5
+                https://test-api.reportnet.europa.eu?API_KEY=502982a2-95a5-43ae-bf3b-d16356042c86
+                https://test-api.reportnet.europa.eu?VERSION=1&API_KEY=502982a2-95a5-43ae-bf3b-d16356042c86&PROVIDER_ID=5
+                https://test-api.reportnet.europa.eu?VERSION=2&API_KEY=502982a2-95a5-43ae-bf3b-d16356042c86&DATAFLOW_ID=861&PROVIDER_ID=10
     """
     from fmeobjects import FMEException
     from fmewebservices import FMENamedConnectionManager
